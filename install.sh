5ed7d1f62d634cf615cd6ce1ec2f282fe0c00368
Nicholas Lediet <nicklediet@gmail.com>
2024-06-17 23:00:13 -0400

    Looping over backup file test

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 REPO_PATH="${HOME}/.lil-dotties"
 SCRIPTS_PATH="${HOME}/.lil-scripts"
+BACKUP_PATH="${HOME}/.lil-backups"
 
 # Bring in lib code
 function load-lib-code {
@@ -10,21 +11,48 @@ function load-lib-code {
 	done
 }
 
-#printf "Please enter your git credentials for"
-
 function config {
-   git --git-dir="${REPO_PATH}" --work-tree=$HOME $@
+   git --git-dir="${REPO_PATH}" --work-tree="${HOME}" $@
 }
 
+function clone-bare-repo {
+	git clone --bare git@github.com:NickLediet/lil-dotties.git "${REPO_PATH}"
+}
+
+function create-backup {
+	if [ ! -d "${BACKUP_PATH}" ]; then
+		mkdir -p "${BACKUP_PATH}"
+	fi
+
+	FILE_HASH=$(date +%s)
+	CURRENT_BACKUP_SUBDIR="${BACKUP_PATH}/${FILE_HASH}-$(whoami)-backup"
+
+	mkdir "${CURRENT_BACKUP_SUBDIR}"
+	FILES=$(config checkout 2>&1 | egrep "\s+\." | awk {'print $1'})
+	for FILE_PATH in "${FILES}"; do
+		echo "${FILE_PATH}"
+	done
+#	xargs -I{} mkdir -p {} "${CURRENT_BACKUP_SUBDIR}"/{} 
+#	xargs -I{} mv {} "${CURRENT_BACKUP_SUBDIR}"/{} 
+}
 
 function _install {
-	yesno_exit "Do you wish to run the install script for lil-dotties?" 
-	config status
-	echo 'Worked'
-	prompt-git-creds
+	clone-bare-repo
+	config config status.showUntrackedFiles yes
+	load-lib-code
+	yesno_exit "Do you wish to run the install script for lil-dotties?"
+	# Verify if changes have been made and create backups if required
+	config checkout 
+	if [ $? = 0 ]; then
+		echo "Checked out configuration."
+	else
+		echo "Backing up existing dotfiles"
+		create-backup
+	fi
+	#config checkout
+	#config config status.showUntrackedFiles no
 		
 }
-load-lib-code
 _install
 
 # Todo:
