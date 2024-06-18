dad5bbb2a3e50e989e1668a2b6b02f4d09dfa986
Nicholas Lediet <nicklediet@gmail.com>
2024-06-17 23:00:13 -0400

    Add baackup logic and cleanup install script

--- install.sh
+++ install.sh
@@ -17,31 +17,37 @@ function config {
 
 function clone-bare-repo {
 	git clone --bare https://github.com/NickLediet/lil-dotties.git "${REPO_PATH}"
-	#git clone --bare git@github.com:NickLediet/lil-dotties.git "${REPO_PATH}"
 }
 
 function create-backup {
+	# Create backup directory if it isn't defined
 	if [ ! -d "${BACKUP_PATH}" ]; then
 		mkdir -p "${BACKUP_PATH}"
 	fi
 
+	# Timestamp for backup
 	FILE_HASH=$(date +%s)
+	# Timestaped & user id'd subdirectory for this particular backup
 	CURRENT_BACKUP_SUBDIR="${BACKUP_PATH}/${FILE_HASH}-$(whoami)-backup"
 
+	# Create the directory for this backup
 	mkdir "${CURRENT_BACKUP_SUBDIR}"
+
+	# Get any unchecked files for backing up (This only checks files that conflict with lil-dotties)
 	FILES=$(config checkout 2>&1 | egrep "\s+\." | awk {'print $1'})
 	for FILE_PATH in "${FILES}"; do
-		echo "${FILE_PATH}"
+		# Back em up!
+		mv "${FILE_PATH}" "${CURRENT_BACKUP_SUBDIR}"
 	done
-#	xargs -I{} mkdir -p {} "${CURRENT_BACKUP_SUBDIR}"/{} 
-#	xargs -I{} mv {} "${CURRENT_BACKUP_SUBDIR}"/{} 
 }
 
 function _install {
 	clone-bare-repo
 	config config --unset status.showUntrackedFiles
 	load-lib-code
+
 #	yesno_exit "Do you wish to run the install script for lil-dotties?"
+
 	# Verify if changes have been made and create backups if required
 	config checkout 
 	if [ $? = 0 ]; then
@@ -55,26 +61,4 @@ function _install {
 		
 }
 _install
-
-# Todo:
-# - [ ] Prove install script works (First GREEN)
-# - [ ] Add install script to repo
-# - [ ] Write actual curl & clone logic for installation script
-
-# # Clone as a bare repo at ~/.lil-dotties
-# git clone --bare git@github.com:NickLediet/lil-dotties.git "${REPO_PATH}"
-
-# # Typically `config checkout` is an expensive do nothing command (according to `man git checkout` at least
-# # In this case we're checking for the status code of doging that to see if there were issues pulling and this creating a back up
-# config checkout
-# if [ $? = 0 ]; then 
-# 	echo "Checked out lil dotties"
-# else
-#     echo "Backing up pre-existing dot files.";
-#     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-# fi;
-# config checkout 
-# config config status.showUntrackedFiles no
-
-
 
