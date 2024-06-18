4ca14088905aca2f6f253c97a7fd6c641c6988e6
Nicholas Lediet <nicklediet@gmail.com>
2024-06-18 00:09:37 -0400

    Add test_unit commands

--- .lil-src/entrypoint.sh
+++ .lil-src/entrypoint.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 curl -L https://raw.githubusercontent.com/NickLediet/lil-dotties/main/install.sh | bash
+
+curl -s https://raw.githubusercontent.com/pgrange/bash_unit/master/install.sh | bash
+./bash_unit -p fail_fails -p assert ~/.lil-src/tests/*
