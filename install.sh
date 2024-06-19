4b2b4a0b3e25445e82727187a757b2781a5de7d1
Nicholas Lediet <nicklediet@gmail.com>
2024-06-19 01:14:50 -0400

    Trying new path for yq installation

--- install.sh
+++ install.sh
@@ -120,7 +120,7 @@ EOF
 		Linux)
 			VERSION='v4.44.2'
 			BINARY='yq_linux_amd64'
-			wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} -O /usr/bin/yq && chmod +x /usr/bin/yq
+			wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} -O /usr/local/bin/yq && chmod +x /usr/local/bin/yq
 			;;
 	esac
 
