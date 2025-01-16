5c7b2d9e179fc98d3b1325e7f15ceeb3ea137854
Nicholas Lediet <nicklediet@gmail.com>
2025-01-16 00:03:56 -0500

    update configs

--- .config/nvim/lua/custom/opts/lsp/jdtls.lua
+++ .config/nvim/lua/custom/opts/lsp/jdtls.lua
@@ -10,79 +10,82 @@ end
 
 
 return {
+  -- See for config options: https://github.com/eclipse-jdtls/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line
+  cmd = {
+    "java",
+    "--version",
+    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+    "-Dosgi.bundles.defaultStartLevel=4",
+    "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    "-Dlog.protocol=true",
+    "-Dlog.level=ALL",
+    "-javaagent:" .. lombokJarPath,
+    "-Xmx1g",
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens",
+    "java.base/java.util=ALL-UNNAMED",
+    "--add-opens",
+    "java.base/java.lang=ALL-UNNAMED",
+
+    "-jar",
+    vim.fn.glob(jdtlsPath .. "/plugins/org.eclipse.equinox.launcher.jar", true),
+
+    "-configuration",
+    jdtlsPath .. "/config_mac_arm",
+
+    "-data",
+    jdtlsData,
+  },
   setings = {
-    -- cmd = {
-    --   "java",
-    --   "--version",
-    --   "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-    --   "-Dosgi.bundles.defaultStartLevel=4",
-    --   "-Declipse.product=org.eclipse.jdt.ls.core.product",
-    --   "-Dlog.protocol=true",
-    --   "-Dlog.level=ALL",
-    --   "-javaagent:" .. lombokJarPath,
-    --   "-Xmx1g",
-    --   "--add-modules=ALL-SYSTEM",
-    --   "--add-opens",
-    --   "java.base/java.util=ALL-UNNAMED",
-    --   "--add-opens",
-    --   "java.base/java.lang=ALL-UNNAMED",
-    --
-    --   "-jar",
-    --   vim.fn.glob(jdtlsPath .. "/plugins/org.eclipse.equinox.launcher.jar", true),
-    --
-    --   "-configuration",
-    --   jdtlsPath .. "/config_mac_arm",
-    --
-    --   "-data",
-    --   jdtlsData,
-    -- },
-    configuration =  {
-      runtimes = {
-        {
-          name = "JavaSE-21",
-          path = getJdkBinPath('temurin-21'),
-          default = true
+    java = {
+      configuration = {
+        home = getJdkBinPath('temurin-21'),
+        runtimes = {
+          {
+            name = "JavaSE-21",
+            path = getJdkBinPath('temurin-21'),
+            default = true
+          },
+          -- {
+          --   name = "JavaSE-17",
+          --   path = getJdkBinPath('temurin-17'),
+          -- },
         },
-        {
-          name = "JavaSE-17",
-          path = getJdkBinPath('temurin-17'),
-          default = false
+        maven = {
+          downloadSources = true,
+          updateSnapshots = true,
+         },
+        referencesCodeLens = {
+          enabled = true,
+        },
+        references = {
+          includeDecompiledSources = true,
+        },
+        -- TODO: update to use intellij formatting
+        format = {
+          enabled = false,
+        },
+        inlayHints = {
+          parameterNames = {
+            enabled = 'all', -- literals, all, none
+          },
         },
       },
-      -- maven = {
-      --   downloadSources = true,
-      --   updateSnapshots = true,
-      --  },
-      -- referencesCodeLens = {
-      --   enabled = true,
-      -- },
-      -- references = {
-      --   includeDecompiledSources = true,
-      -- },
-      -- -- TODO: update to use intellij formatting
-      -- format = {
-      --   enabled = false,
-      -- },
-      -- inlayHints = {
-      --   parameterNames = {
-      --     enabled = 'all', -- literals, all, none
+    },
+      signatureHelp = { enabled = true },
+      -- -- extendedClientCapabilities = extendedClientCapabilities,
+      -- completion = {
+      --   favoriteStaticMembers = {
+      --       "org.hamcrest.MatcherAssert.assertThat",
+      --       "org.hamcrest.Matchers.*",
+      --       "org.hamcrest.CoreMatchers.*",
+      --       "org.junit.jupiter.api.Assertions.*",
+      --       "java.util.Objects.requireNonNull",
+      --       "java.util.Objects.requireNonNullElse",
+      --       "org.mockito.Mockito.*",
       --   },
       -- },
-    },
-    -- signatureHelp = { enabled = true },
-    -- -- extendedClientCapabilities = extendedClientCapabilities,
-    -- completion = {
-    --   favoriteStaticMembers = {
-    --       "org.hamcrest.MatcherAssert.assertThat",
-    --       "org.hamcrest.Matchers.*",
-    --       "org.hamcrest.CoreMatchers.*",
-    --       "org.junit.jupiter.api.Assertions.*",
-    --       "java.util.Objects.requireNonNull",
-    --       "java.util.Objects.requireNonNullElse",
-    --       "org.mockito.Mockito.*",
-    --   },
-    -- },
-  }
+    }
 }
 
 
