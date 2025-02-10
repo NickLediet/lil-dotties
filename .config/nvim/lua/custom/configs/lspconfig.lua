63d2e506405393a3841ac9615f11fe6642183744
Nicholas Lediet <nicklediet@gmail.com>
2025-02-10 15:58:34 -0500

    Commit before trying to reset nvim

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,19 +1,42 @@
-  -- Reserve a space in the gutter
-  -- This will avoid an annoying layout shift in the screen
-  vim.opt.signcolumn = 'yes'
+local nvlsp = require "nvchad.configs.lspconfig"
+local lspconfig = require "lspconfig"
 
-  -- Add cmp_nvim_lsp capabilities settings to lspconfig
-  -- This should be executed before you configure any language server
-  local lspconfig_defaults = require('lspconfig').util.default_config
-  lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-    'force',
-    lspconfig_defaults.capabilities,
-    require('cmp_nvim_lsp').default_capabilities()
-  )
+nvlsp.defaults() -- load nvchad's defaults
 
-  require('lspconfig').jdtls.setup((function()
-    print('lspconf is being loaded')
-    -- FYI!: java/nvim-java needs to be setup before jdtls
-    require('java').setup(require('custom.opts.java'))
-    return require('custom.opts.lsp.jdtls')
-  end)())
+local servers = {
+  "html",
+  "lua",
+  "cssls",
+  -- TypeScript
+  "ts_ls"
+}
+
+-- initialize all lsp servers dynamically
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities
+  }
+end
+
+-- OLD CONFIGS BELOW 
+-- -- Reserve a space in the gutter
+  -- -- This will avoid an annoying layout shift in the screen
+  -- vim.opt.signcolumn = 'yes'
+  --
+  -- -- Add cmp_nvim_lsp capabilities settings to lspconfig
+  -- -- This should be executed before you configure any language server
+  -- local lspconfig_defaults = require('lspconfig').util.default_config
+  -- lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+  --   'force',
+  --   lspconfig_defaults.capabilities,
+  --   require('cmp_nvim_lsp').default_capabilities()
+  -- )
+  --
+  -- require('lspconfig').jdtls.setup((function()
+  --   print('lspconf is being loaded')
+  --   -- FYI!: java/nvim-java needs to be setup before jdtls
+  --   require('java').setup(require('custom.opts.java'))
+  --   return require('custom.opts.lsp.jdtls')
+  -- end)())
--- .config/nvim/lua/custom/plugins/lspconfig/init.lua
+++ .config/nvim/lua/custom/plugins/lspconfig/init.lua
@@ -1,4 +1,10 @@
 return {
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
+  -- {'hrsh7th/cmp-nvim-lsp'},
+  -- {'hrsh7th/nvim-cmp'},
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "custom.configs.lspconfig"
+    end,
+  }
 }
